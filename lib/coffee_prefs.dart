3c8fd64d292afce5c15400eee5fc950f8037f206
mohammad.omar.317 <mohammad.omar.317@gmail.com>
2024-05-05 22:58:29 +0000

    ✨ #13 - Images

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CoffeePrefs extends StatelessWidget {
   const CoffeePrefs({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
         Row(
           children: [
-            Text("Strength: "),
-            Text("3"),
-            SizedBox(
+            const Text("Strength: "),
+            const Text("3"),
+            Image.asset(
+              "assets/img/coffee_bean.png",
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
+            ),
+            const SizedBox(
               width: 50,
             )
           ],
         ),
         Row(
           children: [
-            Text("Sugars: "),
-            Text("3"),
-            SizedBox(
+            const Text("Sugars: "),
+            const Text("3"),
+            Image.asset(
+              "assets/img/sugar_cube.png",
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
+            ),
+            const SizedBox(
               width: 50,
             )
           ],
