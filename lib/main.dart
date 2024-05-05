374deb1fcc03ed01d027140ee29134182a391bc1
mohammad.omar.317 <mohammad.omar.317@gmail.com>
2024-05-05 07:52:20 +0000

    ✨ #11 - Columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/home.dart';
-
+import 'package:myapp/sandbox.dart';
 
 void main() {
   runApp(const MaterialApp(home: Home()));
---
+++ lib/sandbox.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sandbox"),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            color: Colors.red,
+            child: const Text("one"),
+          ),
+          Container(
+            width: 200,
+            color: Colors.green,
+            child: const Text("two"),
+          ),
+          Container(
+            width: 300,
+            color: Colors.blue,
+            child: const Text("three"),
+          ),
+        ],
+      ),
+    );
+  }
+}
